a6126cc3d8c140908c303751b134985cbecf0b1f
Julius Bierbaum <julius.bierbaum@fh-bielefeld.de>
2016-05-06 20:49:42 +0200

    Add some navigation for testing

--- src/js/pages/IndexPage.js
+++ src/js/pages/IndexPage.js
@@ -4,6 +4,10 @@ export default class IndexPage extends React.Component {
     render() {
         return (
             <div>
+                <nav>
+                    <a href="#/">Home</a><br />
+                    <a href="#/anotherhelloworld">Another Hello World</a>
+                </nav>
                 {this.props.children}
             </div>
         );
