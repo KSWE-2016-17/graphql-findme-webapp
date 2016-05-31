f70fe3833b0695ad28f09399a457487767eb43ea
Julius Bierbaum <julius.bierbaum@fh-bielefeld.de>
2016-05-31 15:38:47 +0200

    add component OtherProfileView_RestrictedRow
    This is just a single line of text that tells the user he's got only a
    limited view on the data of that profile.

---
+++ src/js/components/OtherProfileView_RestrictedRow.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default class OtherProfileView_RestrictedRow extends React.Component {
+	
+    create() {
+        return (
+		    <div className="container">
+			    <h2 style={{textAlign:"center"}}>Dieses Profil ist f&uuml;r dich nur beschr&auml;nkt sichtbar.</h2>
+		    </div>
+		);
+    }
+
+    render() {
+        return (
+            <div>{this.create()}</div>
+        );
+    }
+}
