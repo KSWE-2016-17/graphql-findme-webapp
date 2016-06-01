8a20e14a748353d5f58c71c9c9ea73717ab32015
Mirko Kreitz <mirko_weidemann.kreitz@fh-bielefeld.de>
2016-06-01 02:23:19 +0200

    Implement ProfileinstellungenViewPage and components

---
+++ src/js/components/ProfileinstellungenViewPrivatsphaereComponent.js
@@ -0,0 +1,65 @@
+import React from "react";
+
+import DefaultProfilImage from "./DefaultProfilImage";
+
+export default class ProfileinstellungenViewPrivatsphaereComponent extends React.Component {
+
+    createProfileinstellungenViewPrivatsphaereComponent() {
+        return( <div className="container">
+            <div className="row">
+                <h3>Privatsph&auml;re</h3>
+                <div className="col-md-3">
+                    <br/>
+                    <div className="checkbox">
+                        <label><input type="checkbox" value=""/>Checkbox 1</label>
+                    </div>
+                    <div className="checkbox">
+                        <label><input type="checkbox" value=""/>Checkbox 2</label>
+                    </div>
+                    <div className="checkbox">
+                        <label><input type="checkbox" value=""/>Checkbox 3</label>
+                    </div>
+                </div>
+                <div className="col-md-3">
+                    <br/>
+                    <div className="checkbox">
+                        <label><input type="checkbox" value=""/>Checkbox 4</label>
+                    </div>
+                    <div className="checkbox">
+                        <label><input type="checkbox" value=""/>Checkbox 5</label>
+                    </div>
+                    <div className="checkbox">
+                        <label><input type="checkbox" value=""/>Checkbox 6</label>
+                    </div>
+                </div>
+                <div className="col-md-3">
+                    <span>Onlinestatus sichtbar f&uuml;r:</span>
+                    <div class="checkbox">
+                        <label><input type="radio" name="optradio"/> Alle</label>
+                    </div>
+                    <div class="checkbox">
+                        <label><input type="radio" name="optradio"/> Nur Freunde</label>
+                    </div>
+                    <div class="checkbox">
+                        <label><input type="radio" name="optradio"/> niemanden</label>
+                    </div>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-md-12">
+                    <br/>
+                    <button className="btn btn-primary btn-md" type="button">
+                        <span className="glyphicon glyphicon-lock"></span> speichern</button>
+                </div>
+            </div>
+            <hr/>
+        </div>);
+    }
+
+    render() {
+        return (
+            <div>{this.createProfileinstellungenViewPrivatsphaereComponent()}
+            </div>
+        );
+    }
+}
---
+++ src/js/pages/ProfileinstellungenViewPage.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import ProfilAnsichtNavigationElement from "../components/ProfilAnsichtNavigationElement";
+import ProfileinstellungenViewPrivatsphaereComponent from "../components/ProfileinstellungenViewPrivatsphaereComponent";
+import ProfileinstellungenViewSonstigesComponent from "../components/ProfileinstellungenViewSonstigesComponent";
+import ProfileinstellungenViewProfilLoeschenComponent from "../components/ProfileinstellungenViewProfilLoeschenComponent";
+
+export default class ProfileinstellungenViewPage extends React.Component {
+
+    createProfileinstellungenViewPage() {
+        return(  <div>
+            <ProfilAnsichtNavigationElement/>
+            <div className="container">
+                <ProfileinstellungenViewPrivatsphaereComponent/>
+                <ProfileinstellungenViewSonstigesComponent/>
+                <ProfileinstellungenViewProfilLoeschenComponent/>
+            </div>
+        </div>);
+    }
+
+    render() {
+        return (
+            <div>{this.createProfileinstellungenViewPage()}</div>
+        );
+    }
+}
