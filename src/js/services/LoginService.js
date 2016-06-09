16fc74f4436d2b9bb99f11c36dbf4dfd0f8f5e33
Alexej Esau <alexej.esau@fh-bielefeld.de>
2016-06-09 22:09:47 +0200

    Use the couchdb API for login

--- src/js/services/LoginService.js
+++ src/js/services/LoginService.js
@@ -4,36 +4,27 @@ import connSettings from "../../conn-settings";
 
 export default class LoginService {
     login(login, password, callbacks) {
-        if (login === "foobar@foo.bar" && password === "foobar") {
-            if (callbacks && typeof callbacks.success === "function") {
-                callbacks.success("login ok");
+        let dm = new CouchDbApi.DaoManager(connSettings);
+        let userDao = dm.getDao(CouchDbApi.UserDAO);
+        userDao.findByLogin(login, {
+            success: function(data) {
+                if (data && data.rows[0].value.password === password) {
+                    if (callbacks && typeof callbacks.success === "function") {
+                        localStorage.setItem("sessionUser", data.rows[0].value);
+                        callbacks.success(data);
+                    }
+                } else {
+                    if (callbacks && typeof callbacks.error === "function") {
+                        callbacks.error("wrong username or password");
+                    }
+                }
+            },
+            error: function(err) {
+                console.error(err);
+                if (callbacks && typeof callbacks.error === "function") {
+                    callbacks.error(err);
+                }
             }
-        } else {
-            if (callbacks && typeof callbacks.error === "function") {
-                callbacks.error("wrong username or password");
-            }
-        }
-
-        // let dm = new CouchDbApi.DaoManager(connSettings);
-        // let userDao = dm.getDao(CouchDbApi.UserDAO);
-        // userDao.findByLogin(login, {
-        //     success: function(data) {
-        //         if (data && data.password === password) {
-        //             if (callbacks && typeof callbacks.success === "function") {
-        //                 callbacks.success(data);
-        //             }
-        //         } else {
-        //             if (callbacks && typeof callbacks.error === "function") {
-        //                 callbacks.error("wrong username or password");
-        //             }
-        //         }
-        //     },
-        //     error: function(err) {
-        //         console.error(err);
-        //         if (callbacks && typeof callbacks.error === "function") {
-        //             callbacks.error(err);
-        //         }
-        //     }
-        // });
+        });
     }
 }
