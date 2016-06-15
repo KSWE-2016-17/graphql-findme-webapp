b34a8405a39b0fbb4c38e5dadd7d1ee3b9d4d646
Julius Bierbaum <julius.bierbaum@fh-bielefeld.de>
2016-06-15 05:12:38 +0200

    Give functionality to friends list
    The component FriendsListTab_FrindsListRow now uses a service component to
    gain all friend profiles of the user that is currently logged in.
    Then for every friend entry from database this component creates a new
    FriendsListTab_FriendRow instance to show info about a user.
    
    Because the used service component can't deliver the needed information
    (for now), this component contains some hard-coded friends.

--- src/js/components/FriendsListTab_FriendsListRow.js
+++ src/js/components/FriendsListTab_FriendsListRow.js
@@ -1,29 +1,54 @@
 import React from "react";
 
 import FriendRow from "./FriendsListTab_FriendRow";
+import FriendsListService from "../services/FriendsListService";
 
 export default class FriendProfileView_HeadRow extends React.Component {
 
+	loadFriendsList(callback) {
+        let friendsListService = new FriendsListService();
+        friendsListService.allFriends({
+            success: function(data) {
+				callback.success(data);
+            },
+            error: function(err) {
+				callback.error(data);
+                console.log(err);
+            }
+        });
+    }
+
     create() {
-        return (
+		var friends = [{id:"xxx", status:"0"}, {id:"xxy", status:"1"}, {id:"xxz", status:"2"}]; // this data is should come from database
+		
+		this.loadFriendsList({
+			success: function(data) {
+				friends = data;
+			}
+			/*error: function(err) {
+				return (
+					<div className="container">
+					</div>
+				);
+            }*/
+		});
+		
+		return (
 		    <div className="container">
 			    <br />
-                <div className="row">
-				    <FriendRow />
-				</div>
-				<hr />
-				<div className="row">
-				    <FriendRow />
-				</div>
-				<hr />
-				<div className="row">
-				    <FriendRow />
-				</div>
-				<hr />
-				<div className="row">
-				    <FriendRow />
-				</div>
-				<hr />
+				
+				{friends.map(function(obj, index){
+					if (obj.status == "2") {
+						return;
+					}
+					return (
+						<div className="row">
+							<FriendRow friend_id={obj.id} friend_status={obj.status}/>
+							<hr />
+						</div>
+					);
+                })}
+				
             </div>
 		);
     }
