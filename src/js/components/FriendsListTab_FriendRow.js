d276af27780b80ce03bbcac55a6f0bab629059d2
Julius Bierbaum <julius.bierbaum@fh-bielefeld.de>
2016-06-22 04:45:03 +0200

    Extend functionality of friend row
    All use-cases are implemented, but some of the used profile ids
    are still hard-coded

--- src/js/components/FriendsListTab_FriendRow.js
+++ src/js/components/FriendsListTab_FriendRow.js
@@ -1,99 +1,116 @@
 import React from "react";
+import q from "q";
+import _ from "lodash";
 
 import Image from "./DefaultProfilImage";
+import FriendsListService from "../services/FriendsListService";
 
 export default class FriendsListTab_FriendRow extends React.Component {
 	
-	// properties set: friend_status
-	//                 friend_id
-	
+	constructor(props) {
+        super(props);
+
+        this.state = {
+        };
+    }
 	
 	addFriend() {
-		// not implemented
+		let friendsListService = new FriendsListService();
+		// this.props.friendsListID, this.props.profileID
+		friendsListService.handleFriendRequest("48cf296b53896d16da217994e004e5a3", "48cf296b53896d16da217994e0037163", true);
 		window.location.href = "#/friendstab"; // so the view gets updated
 	}
 	
 	dontAddFriend() {
-		// not implemented
+		let friendsListService = new FriendsListService();
+		// this.props.friendsListID, this.props.profileID
+		friendsListService.handleFriendRequest("48cf296b53896d16da217994e004e5a3", "48cf296b53896d16da217994e0037163", false);
 		window.location.href = "#/friendstab"; // so the view gets updated
 	}
 	
 	endFriendship() {
-		// not implemented
+		let friendsListService = new FriendsListService();
+		// this.props.friendsListID, this.props.profileID
+		friendsListService.endFrienship("48cf296b53896d16da217994e004e5a3", "48cf296b53896d16da217994e0037163");
 		window.location.href = "#/friendstab"; // so the view gets updated
 	}
 	
 	reportUser() {
-		// not implemented
+		let friendsListService = new FriendsListService();
+        friendsListService.reportUser(profileID)
 	}
 	
 	sendMessage() {
-		// not implemented
 		window.location.href = "#/mails/new";
 	}
 	
 	showProfile() {
-		// not implemented
-		window.location.href = "#/friends/32";
+		window.location.href = "#/friends/3"; // + this.props.profileID;
 	}
 	
-	getUserName() {
-		// not implemented
-		return "Klaus Meier";
-	}
+	componentDidMount() {
+        let self = this;
+        let friendsListService = new FriendsListService();
 
-    create() {
-		if (this.props.friend_status === "1") {
+		friendsListService.getProfile(self.props.profileID)
+            .then(function(data) {
+                friendsListService.getUser(data[0].user_id)
+					.then(function(data) {
+						self.setState({friendName: data[0].login});
+					})
+					.catch(function(err) {
+						console.log(err);
+					});
+            })
+            .catch(function(err) {
+                console.log(err);
+            });
+    }
+	
+	render() {
+        let self = this;
+
+		if (this.props.status === 0) { // friend request
 			return (
-				<div className="container">
-					<div className="row">
-						<div className="col-md-1">
-							<Image />
-						</div>
-						<div className="col-md-11">
-							<font size="5"> {this.getUserName()}</font><br />
-							<button type="button" className="btn btn-primary" onClick={this.addFriend} style={{marginLeft:"25px", paddingLeft:"10px", paddingRight:"10px"}}>
-								<span className="glyphicon glyphicon-plus"></span> Freundschaftsanfrage annehmen
-							</button>
-							<button type="button" className="btn btn-primary" onClick={this.dontAddFriend} style={{marginLeft:"25px", paddingLeft:"10px", paddingRight:"10px"}}>
-								<span className="glyphicon glyphicon-remove"></span> Freundschaftsanfrage ablehnen
-							</button>
-						</div>
+				<div className="row">
+					<div className="col-md-1">
+						<Image />
+					</div>
+					<div className="col-md-11">
+						<font size="5"> {self.state.friendName} </font><br />
+						<button type="button" className="btn btn-link" onClick={this.addFriend} style={{marginLeft:"25px", paddingLeft:"10px", paddingRight:"10px"}}>
+							<span className="glyphicon glyphicon-plus"></span> Freundschaftsanfrage annehmen
+						</button>
+						<button type="button" className="btn btn-link" onClick={this.dontAddFriend} style={{marginLeft:"25px", paddingLeft:"10px", paddingRight:"10px"}}>
+							<span className="glyphicon glyphicon-remove"></span> Freundschaftsanfrage ablehnen
+						</button>
 					</div>
 				</div>
 			);
 		}
         else {
 			return (
-				<div className="container">
-					<div className="row">
-						<div className="col-md-1">
-							<Image />
-						</div>
-						<div className="col-md-11">
-							<font size="5"> {this.getUserName()}</font><br />
-							<button type="button" className="btn btn-link" onClick={this.showProfile} style={{marginLeft:"25px", paddingLeft:"0px", paddingRight:"0px"}}>
-								<span className="glyphicon glyphicon-eye-open"></span> Profil ansehen
-							</button>
-							<button type="button" className="btn btn-link" onClick={this.sendMessage} style={{marginLeft:"25px", paddingLeft:"0px", paddingRight:"0px"}}>
-								<span className="glyphicon glyphicon-envelope"></span> Nachricht senden
-							</button>
-							<button type="button" className="btn btn-link" onClick={this.reportUser} style={{marginLeft:"25px", paddingLeft:"0px", paddingRight:"0px"}}>
-								<span className="glyphicon glyphicon-screenshot"></span> Benutzer melden
-							</button>
-							<button type="button" className="btn btn-link" onClick={this.endFriendship} style={{marginLeft:"25px", paddingLeft:"0px", paddingRight:"0px"}}>
-								<span className="glyphicon glyphicon-trash"></span> Freundschaft beenden
-							</button>
-						</div>
+				<div className="row">
+					<div className="col-md-1">
+						<Image />
+					</div>
+					<div className="col-md-11">
+						<font size="5"> {self.state.friendName} </font><br />
+						<button type="button" className="btn btn-link" onClick={this.showProfile} style={{marginLeft:"25px", paddingLeft:"0px", paddingRight:"0px"}}>
+							<span className="glyphicon glyphicon-eye-open"></span> Profil ansehen
+						</button>
+						<button type="button" className="btn btn-link" onClick={this.sendMessage} style={{marginLeft:"25px", paddingLeft:"0px", paddingRight:"0px"}}>
+							<span className="glyphicon glyphicon-envelope"></span> Nachricht senden
+						</button>
+						<button type="button" className="btn btn-link" onClick={this.reportUser} style={{marginLeft:"25px", paddingLeft:"0px", paddingRight:"0px"}}>
+							<span className="glyphicon glyphicon-screenshot"></span> Benutzer melden
+						</button>
+						<button type="button" className="btn btn-link" onClick={this.endFriendship} style={{marginLeft:"25px", paddingLeft:"0px", paddingRight:"0px"}}>
+							<span className="glyphicon glyphicon-trash"></span> Freundschaft beenden
+						</button>
 					</div>
 				</div>
 			);
 		}
-    }
-
-    render() {
-        return (
-            <div>{this.create()}</div>
-        );
     }
 }
