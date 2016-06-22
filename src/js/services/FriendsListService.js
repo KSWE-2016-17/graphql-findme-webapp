5f5e2a91c65f01cc8af549800c2e8650933d8de6
Julius Bierbaum <julius.bierbaum@fh-bielefeld.de>
2016-06-22 04:34:56 +0200

    extend service FriendsListService
    All required services for the friends list are now available.
     - get all friends by profile id
     - get profile id of the user currently logged in
     - get profile object of the given profile id
     - report a profile
     - end friendship
     - handle friend request

--- src/js/services/FriendsListService.js
+++ src/js/services/FriendsListService.js
@@ -1,55 +1,120 @@
+import q from "q";
+import _ from "lodash";
+
 import CouchDbApi from "findme-react-couchdb-api";
 
 import connSettings from "../../conn-settings";
 
 export default class FriendsListService {
-    allFriends(callbacks) {
+    allFriends(profileID) {	
+		let defer = q.defer();
+
         let dm = new CouchDbApi.DaoManager(connSettings);
         let friendDao = dm.getDao(CouchDbApi.FriendDAO);
-		//console.log("session user id: " + localStorage.getItem("sessionUser")._id); // for debugging. the value is undefined all the time...
-        friendDao.findByProfileId(localStorage.getItem("sessionUser")._id, { // for some reason this localStorage query doesn't return a value
+
+        friendDao.findByProfileId(profileID)
+            .then(defer.resolve)
+            .catch(defer.reject);
+
+        return defer.promise;
+    }
+	
+	getCurrentProfile() {
+		let defer = q.defer();
+
+        let dm = new CouchDbApi.DaoManager(connSettings);
+        let profileDao = dm.getDao(CouchDbApi.ProfileDAO);
+
+		profileDao.findByUserId(localStorage.getItem("sessionUserId"))
+            .then(defer.resolve)
+            .catch(defer.reject);
+
+        return defer.promise;
+	}
+	
+	getProfile(profileID) {
+		let defer = q.defer();
+
+        let dm = new CouchDbApi.DaoManager(connSettings);
+        let profileDao = dm.getDao(CouchDbApi.ProfileDAO);
+
+        profileDao.findById(profileID)
+            .then(defer.resolve)
+            .catch(defer.reject);
+
+        return defer.promise;
+	}
+	
+	getUser(userID) {
+		let defer = q.defer();
+
+        let dm = new CouchDbApi.DaoManager(connSettings);
+        let userDao = dm.getDao(CouchDbApi.UserDAO);
+
+        userDao.findById(userID)
+            .then(defer.resolve)
+            .catch(defer.reject);
+
+        return defer.promise;
+	}
+	
+	reportUser(profileID) {
+        let dm = new CouchDbApi.DaoManager(connSettings);
+        let profileDao = dm.getDao(CouchDbApi.ProfileDAO);
+
+        profileDao.findByUserId(profileID,  {
             success: function(data) {
                 if (data) {
-                    if (callbacks && typeof callbacks.success === "function") {
-						//console.log("friend id #1: " + data[0].friends[0].id); // for debugging.
-                        callbacks.success(data[0].friends);
-                    }
-                } else {
-                    if (callbacks && typeof callbacks.error === "function") {
-                        callbacks.error("error");
-                    }
+                    data[0].reported = "true";
+					profileDao.update(data[0], {
+					});
                 }
-            },
-            error: function(err) {
-                console.error(err);
-                if (callbacks && typeof callbacks.error === "function") {
-                    callbacks.error(err);
-                }
+            }
+        });
+	}
+	
+	endFrienship(friendsListID, profileID) {
+		let dm = new CouchDbApi.DaoManager(connSettings);
+        let friendDao = dm.getDao(CouchDbApi.FriendDAO);
+
+        friendDao.findById(friendsListID,  {
+            success: function(data) {
+                let friendsList = data[0].friends;
+				let newFriendsList = [];
+				
+				for (let i = 0; i < friendsList.length; i++) {
+					if (friendsList[i].id != profileID) {
+						newFriendsList.push(friendsList[i]);
+					}
+				}
+				
+				data[0].friends = newFriendsList;
+				
+				friendDao.update(data[0], {
+				});
             }
         });
     }
 	
-	endFrienship(friend_id, callbacks) {
-		/*
-		should do something like:
-		  - remove the friend with the corresponding ID from the Friends.friends map
-		*/
-        // not implemented
-    }
-	
-	acceptFriendRequest(friend_id, callbacks) {
-		/*
-		should do something like:
-		  - change the friend status in the Friends.friends map to 1
-		*/
-        // not implemented
-    }
-	
-	declineFriendRequest(friend_id, callbacks) {
-        /*
-		should do something like:
-		  - change the friend status in the Friends.friends map to 2
-		*/
-        // not implemented
+	handleFriendRequest(friendsListID, profileID, accept) {
+		let dm = new CouchDbApi.DaoManager(connSettings);
+        let friendDao = dm.getDao(CouchDbApi.FriendDAO);
+		
+        friendDao.findById(friendsListID,  {
+            success: function(data) {
+                let friendsList = data[0].friends;
+				
+				for (let i = 0; i < friendsList.length; i++) {
+					if (friendsList[i].id == profileID) {
+						friendsList[i].status = accept == true ? 1 : 2;
+					}
+				}
+				
+				data[0].friends = friendsList;
+				
+				friendDao.update(data[0], {
+				});
+            }
+        });
     }
 }
