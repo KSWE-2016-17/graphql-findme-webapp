6fd6196a510a96d63a5f698401b4f00a0d38492f
ktymann <karsten_michael.tymann@fh-bielefeld.de>
2016-06-29 15:37:58 +0200

    Implement Profilepic, Message Bugfix
    +implemented profilepic
    +fixxed message bug where in archive sent messages
    dont get deleted

--- src/js/components/DefaultImage.js
+++ src/js/components/DefaultImage.js
@@ -1,9 +1,12 @@
 import React from "react";
 
+import PS from "../services/PictureService";
+
 export default class DefaultImageComponent extends React.Component {
     
     createDefaultImage() {
-        return <img src={'http://www.billiardimports.com.au/sites/default/files/imagecache/product_shop/imagefield_default_images/default-no-image_13.png'} alt="default" width="140px" height="140px" className="img-thumbnail"/>;
+
+        return <img src={'http://www.billiardimports.com.au/sites/default/files/imagecache/product_shop/imagefield_default_images/default-no-image_13.png'} alt="default" width="140px" height="140px" className="img-thumbnail" id="pic"/>;
     }
 
     render() {
---
+++ src/js/components/ProfileImage.js
@@ -0,0 +1,61 @@
+import React from "react";
+
+import PS from "../services/PictureService";
+
+export default class ProfileImage extends React.Component {
+
+    createDefaultProfilImage() {
+        let ps = new PS();
+
+
+        ps.findProfileByUserId(localStorage.getItem("sessionUserId"))
+            .then(function(data){
+                ///
+                //
+                ps.getPictureData(localStorage.getItem("sessionProfileId"))
+                    .then(function(data){
+                        var url = "http://elbe203.startdedicated.de:15984/findme/";
+
+                        console.log("BILD");
+                        console.log(data[0]._id);
+                        console.log(data[0]);
+
+                        url = url + data[0]._id + "/";
+
+                        for(var x in data[0]._attachments){
+                            console.log(x);
+                            var foo = data[0]._attachments[x];
+                            console.log(foo);
+                            url = url + x;
+                            break;
+                        }
+
+                        console.log("URL FOLGT");
+
+
+                        console.log(url);
+
+                        $("#pic").attr("src",url);
+
+
+                    })
+                    .catch(function(err){
+                        console.log(err);
+                    });
+                //
+                ///
+            })
+            .catch(function(err){
+                console.log(err);
+            });
+
+
+        return <img src={'https://b.thumbs.redditmedia.com/GVc0WVhrmqvIQyMolx763ItKqJ0krwqZLOw7nlg03uY.jpg'} alt="default" width="140px" height="140px" className="img-thumbnail" id="pic"/>;
+    }
+
+    render() {
+        return (
+            <div>{this.createDefaultProfilImage()}</div>
+        );
+    }
+}
