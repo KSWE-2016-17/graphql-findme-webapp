65984cf15572a02f9d7ffd6736c4de367429cb66
Julius Bierbaum <julius.bierbaum@fh-bielefeld.de>
2016-06-29 15:27:43 +0200

    Add report view page

---
+++ src/js/pages/ReportsView.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import NavigationBar from "../components/ProfilAnsichtNavigationElement";
+import Head from "../components/ReportsView_HeadRow";
+import ReportsList from "../components/ReportsView_ReportsListRow";
+
+export default class FriendsListTab extends React.Component {
+
+    render() {
+        return (
+            <div>
+				<NavigationBar />
+				<Head />
+				<ReportsList />
+            </div>
+        );
+    }
+}
