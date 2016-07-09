aa45cb52770b6ff38adcdbe94044a05a13a3f8e3
Alexej Esau <alexej.esau@fh-bielefeld.de>
2016-07-09 13:44:51 +0200

    Wrap the whole page by a bootstrap container

--- src/js/pages/IndexPage.js
+++ src/js/pages/IndexPage.js
@@ -3,7 +3,7 @@ import React from "react";
 export default class IndexPage extends React.Component {
     render() {
         return (
-            <div>
+            <div className="container-fluid">
                 {this.props.children}
             </div>
         );
