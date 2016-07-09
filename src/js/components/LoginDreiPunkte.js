4a9332cce92a6487db0bf1ab0162d572386606d1
Alexej Esau <alexej.esau@fh-bielefeld.de>
2016-07-09 14:21:37 +0200

    Use teaser elements

--- src/js/components/LoginDreiPunkte.js
+++ src/js/components/LoginDreiPunkte.js
@@ -1,41 +1,35 @@
 import React from "react";
 
+import TeaserComponent from "../components/TeaserComponent";
+
 export default class LoginDreiPunkte extends React.Component {
-    getDescription(){
+    render() {
+        let self = this;
+
+        return (
+            <div>
+                <div className="row">
+                    <div className="col-md-4">
+                        <TeaserComponent header="Interessen" description={self.createRandomDescription()}
+                                         destination="#"/>
+                    </div>
+                    <div className="col-md-4">
+                        <TeaserComponent header="Vorlieben" description={self.createRandomDescription()}
+                                         destination="#"/>
+                    </div>
+                    <div className="col-md-4">
+                        <TeaserComponent header="Vorstellungen" description={self.createRandomDescription()}
+                                         destination="#"/>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    createRandomDescription() {
         return "Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor " +
             "mauris condimentum nibh, ut fermentum massa justo sit amet risus." +
             "Etiam porta sem malesuada magna mollis euismod. Donec sed" +
             "odio dui.";
-    }
-
-    createContent() {
-        return <div>
-            <div className="row">
-                <div className="col-md-4">
-                    <h3 className="title">Interessen</h3>
-                    <p className="para">{this.getDescription()}</p>
-
-                    <button type="button" className="btn btn-link">mehr ...</button>
-                </div>
-                <div className="col-md-4">
-                    <h3 className="title">Vorlieben</h3>
-                    <p className="para">{this.getDescription()}</p>
-
-                    <button type="button" className="btn btn-link">mehr ...</button>
-                </div>
-                <div className="col-md-4">
-                    <h3 className="title">Vorstellungen</h3>
-                    <p className="para">{this.getDescription()}</p>
-
-                    <button type="button" className="btn btn-link">mehr ...</button>
-                </div>
-            </div>
-        </div>;
-    }
-
-    render() {
-        return (
-            <div>{this.createContent()}</div>
-        );
     }
 }
---
+++ src/js/components/TeaserComponent.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default class TeaserComponent extends React.Component {
+    render() {
+        let self = this;
+
+        return (
+            <div>
+                <h3>{self.props.header}</h3>
+                <p>{self.props.description}</p>
+                {(() => {
+                    if (self.props.destination) {
+                        return (<a className="btn btn-link" href={self.props.destination}>mehr ...</a>);
+                    }
+                })()}
+            </div>
+        );
+    }
+}
