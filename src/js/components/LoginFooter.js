ee6cb6fb6f1e585169c9583011b67e210303033a
Alexej Esau <alexej.esau@fh-bielefeld.de>
2016-07-09 12:57:41 +0200

    Refactor LoginFooter component

--- src/js/components/LoginFooter.js
+++ src/js/components/LoginFooter.js
@@ -1,26 +1,14 @@
 import React from "react";
 
 export default class LoginFooter extends React.Component {
-
-    createContent() {
-        return <div>
-            <div className="row">
-                <div className="col-md-12">
-                    <hr  style={{border: "100px red"}}></hr>
-                </div>
-            </div>
-            <div className="row">
-                <div style={{border:"1px solid"}}></div>
-                <div className="col-md-12">
-                    <h5>@find.me 2015</h5>
-                </div>
-            </div>
-        </div>;
-    }
-
     render() {
         return (
-            <div>{this.createContent()}</div>
+            <div className="container">
+                <div className="row">
+                    <hr />
+                    <div className="text-center">@ 2016 find.me Alle Rechte vorbehalten.</div>
+                </div>
+            </div>
         );
     }
 }
