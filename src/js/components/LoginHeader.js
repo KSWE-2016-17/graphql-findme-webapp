5a160906b3a2521143b34309cea81d68b1e2cf20
Alexej Esau <alexej.esau@fh-bielefeld.de>
2016-07-09 14:40:36 +0200

    Refactor LoginHeader component

--- src/js/components/LoginHeader.js
+++ src/js/components/LoginHeader.js
@@ -3,54 +3,54 @@ import React from "react";
 import LoginService from "../services/LoginService";
 
 export default class LoginHeader extends React.Component {
-    login() {
-        let loginService = new LoginService();
-        loginService.login($("#email").val(), $("#password").val(), {
-            success: function(data) {
-                location.href = "#/profile";
-            },
-            error: function(err) {
-                console.log(err);
-            }
-        });
-        
+    constructor(props) {
+        super(props);
+
+        this.login = this.login.bind(this);
     }
-
-    createContent() {
-        return <div>
-            <div className="row"  style={{backgroundColor: "LightGray"}}>
-                <div className="col-md-1">
-                    <h3 className="title">find.me</h3>
-                </div>
-                <form className="form-inline">
-                    <div className="col-md-2 col-md-offset-5">
-                        <div className="input-group">
-                            <div className="form-group">
-                                <input type="email" className="form-control" id="email" placeholder="Email"></input>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-2">
-                        <div className="form-group">
-                            <input type="password" className="form-control" id="password"
-                                   placeholder="Password"></input>
-                        </div>
-                                       </div>
-                                       <div className="col-md-1">
-                        <button type="button" className="btn btn-primary" onClick={this.login}>Anmelden</button>
-                    </div>
-                </form>
-
-            </div>
-
-            </div>;
-    }
-
-
 
     render() {
         return (
-            <div>{this.createContent()}</div>
+            <div>
+                <div className="row" style={{backgroundColor: "LightGray"}}>
+                    <div className="col-md-1">
+                        <h3 className="title">find.me</h3>
+                    </div>
+                    <form className="form-inline">
+                        <div className="col-md-2 col-md-offset-5">
+                            <div className="input-group">
+                                <div className="form-group">
+                                    <input type="email" className="form-control" id="email" placeholder="Email"></input>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-2">
+                            <div className="form-group">
+                                <input type="password" className="form-control" id="password"
+                                       placeholder="Password"></input>
+                            </div>
+                        </div>
+                        <div className="col-md-1">
+                            <button type="button" className="btn btn-primary" onClick={this.login}>Anmelden</button>
+                        </div>
+                    </form>
+
+                </div>
+
+            </div>
         );
+    }
+
+    login() {
+        let loginService = new LoginService();
+
+        loginService.login($("#email").val(), $("#password").val(), {
+            success: function (data) {
+                location.href = "#/profile";
+            },
+            error: function (err) {
+                console.log(err);
+            }
+        });
     }
 }
