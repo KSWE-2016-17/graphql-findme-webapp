88b55a77c2f60832247f2e7a547c4248f903a7ac
Alexej Esau <alexej.esau@fh-bielefeld.de>
2016-07-09 12:48:25 +0200

    Clean up LoginPage component

--- src/js/pages/LoginPage.js
+++ src/js/pages/LoginPage.js
@@ -8,25 +8,20 @@ import LoginFooter from "../components/LoginFooter";
 export default class LoginViewComponent extends React.Component {
     constructor(props) {
         super(props);
+
         if (localStorage.getItem("sessionUserId")) {
             location.href = "#/profile";
         }
     }
 
-    createContent() {
-        return <div className="container">
-            <LoginHeader/>
-            <LoginInfoBox/>
-            <LoginDreiPunkte/>
-            <LoginFooter/>  
-            </div>;
-    }
-
-
-
     render() {
         return (
-            <div>{this.createContent()}</div>
-    );
+            <div className="container">
+                <LoginHeader/>
+                <LoginInfoBox/>
+                <LoginDreiPunkte/>
+                <LoginFooter/>
+            </div>
+        );
     }
 }
