7fbf2fded64d143bd8aca4afde1c5cc26060bda8
Alexej Esau <alexej.esau@fh-bielefeld.de>
2016-07-11 13:25:18 +0200

    Remove dummy content from interests' elements

--- src/js/components/ProfilAnsichtRowInteressen.js
+++ src/js/components/ProfilAnsichtRowInteressen.js
@@ -17,62 +17,52 @@ export default class RowInteressenComponent extends React.Component {
                         <div id="interest0" className="box_interessen" style={{color:"#ffffff", backgroundColor:"#eb9316",
              fontWeight:"bold", borderRadius:"4px", float:"left", paddingTop:"5px",
              paddingBottom:"5px", paddingLeft:"15px", paddingRight:"15px",
-             marginTop:"10px", marginBottom:"10px", marginRight:"10px", marginLeft:"0px"}}>Festivals
-                        </div>
+             marginTop:"10px", marginBottom:"10px", marginRight:"10px", marginLeft:"0px"}}></div>
 
                         <div id="interest1" className="box_interessen" style={{color:"#ffffff", backgroundColor:"#eb9316",
              fontWeight:"bold", borderRadius:"4px", float:"left", paddingTop:"5px",
              paddingBottom:"5px", paddingLeft:"15px", paddingRight:"15px",
-             marginTop:"10px", marginBottom:"10px", marginRight:"10px", marginLeft:"0px"}}>Festivals
-                        </div>
+             marginTop:"10px", marginBottom:"10px", marginRight:"10px", marginLeft:"0px"}}></div>
 
                         <div id="interest2" className="box_interessen" style={{color:"#ffffff", backgroundColor:"#eb9316",
              fontWeight:"bold", borderRadius:"4px", float:"left", paddingTop:"5px",
              paddingBottom:"5px", paddingLeft:"15px", paddingRight:"15px",
-             marginTop:"10px", marginBottom:"10px", marginRight:"10px", marginLeft:"0px"}}>Festivals
-                        </div>
+             marginTop:"10px", marginBottom:"10px", marginRight:"10px", marginLeft:"0px"}}></div>
 
                         <div id="interest3" className="box_interessen" style={{color:"#ffffff", backgroundColor:"#eb9316",
              fontWeight:"bold", borderRadius:"4px", float:"left", paddingTop:"5px",
              paddingBottom:"5px", paddingLeft:"15px", paddingRight:"15px",
-             marginTop:"10px", marginBottom:"10px", marginRight:"10px", marginLeft:"0px"}}>Festivals
-                        </div>
+             marginTop:"10px", marginBottom:"10px", marginRight:"10px", marginLeft:"0px"}}></div>
 
                         <div id="interest4" className="box_interessen" style={{color:"#ffffff", backgroundColor:"#eb9316",
              fontWeight:"bold", borderRadius:"4px", float:"left", paddingTop:"5px",
              paddingBottom:"5px", paddingLeft:"15px", paddingRight:"15px",
-             marginTop:"10px", marginBottom:"10px", marginRight:"10px", marginLeft:"0px"}}>Festivals
-                        </div>
+             marginTop:"10px", marginBottom:"10px", marginRight:"10px", marginLeft:"0px"}}></div>
 
                         <div id="interest5" className="box_interessen" style={{color:"#ffffff", backgroundColor:"#eb9316",
              fontWeight:"bold", borderRadius:"4px", float:"left", paddingTop:"5px",
              paddingBottom:"5px", paddingLeft:"15px", paddingRight:"15px",
-             marginTop:"10px", marginBottom:"10px", marginRight:"10px", marginLeft:"0px"}}>Festivals
-                        </div>
+             marginTop:"10px", marginBottom:"10px", marginRight:"10px", marginLeft:"0px"}}></div>
 
                         <div id="interest6" className="box_interessen" style={{color:"#ffffff", backgroundColor:"#eb9316",
              fontWeight:"bold", borderRadius:"4px", float:"left", paddingTop:"5px",
              paddingBottom:"5px", paddingLeft:"15px", paddingRight:"15px",
-             marginTop:"10px", marginBottom:"10px", marginRight:"10px", marginLeft:"0px"}}>Festivals
-                        </div>
+             marginTop:"10px", marginBottom:"10px", marginRight:"10px", marginLeft:"0px"}}></div>
 
                         <div id="interest7" className="box_interessen" style={{color:"#ffffff", backgroundColor:"#eb9316",
              fontWeight:"bold", borderRadius:"4px", float:"left", paddingTop:"5px",
              paddingBottom:"5px", paddingLeft:"15px", paddingRight:"15px",
-             marginTop:"10px", marginBottom:"10px", marginRight:"10px", marginLeft:"0px"}}>Festivals
-                        </div>
+             marginTop:"10px", marginBottom:"10px", marginRight:"10px", marginLeft:"0px"}}></div>
 
                         <div id="interest8" className="box_interessen" style={{color:"#ffffff", backgroundColor:"#eb9316",
              fontWeight:"bold", borderRadius:"4px", float:"left", paddingTop:"5px",
              paddingBottom:"5px", paddingLeft:"15px", paddingRight:"15px",
-             marginTop:"10px", marginBottom:"10px", marginRight:"10px", marginLeft:"0px"}}>Festivals
-                        </div>
+             marginTop:"10px", marginBottom:"10px", marginRight:"10px", marginLeft:"0px"}}></div>
 
                         <div id="interest9" className="box_interessen" style={{color:"#ffffff", backgroundColor:"#eb9316",
              fontWeight:"bold", borderRadius:"4px", float:"left", paddingTop:"5px",
              paddingBottom:"5px", paddingLeft:"15px", paddingRight:"15px",
-             marginTop:"10px", marginBottom:"10px", marginRight:"10px", marginLeft:"0px"}}>Festivals
-                        </div>
+             marginTop:"10px", marginBottom:"10px", marginRight:"10px", marginLeft:"0px"}}></div>
                     </div>
                 </div>
             </div>
