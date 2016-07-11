18b052f5fd79724619358d6e2abf43fbe2a38acb
Alexej Esau <alexej.esau@fh-bielefeld.de>
2016-07-11 13:22:55 +0200

    Split rendering and data initialization

--- src/js/components/ProfilAnsichtRowInteressen.js
+++ src/js/components/ProfilAnsichtRowInteressen.js
@@ -3,7 +3,83 @@ import React from "react";
 import ProfileService from "../services/ProfilService";
 
 export default class RowInteressenComponent extends React.Component {
-    createRowInteressen() {
+    render() {
+        return (
+            <div>
+                <div className="row">
+                    <div className="col-md-12">
+                        <h1>Interessen</h1>
+                        <br />
+                    </div>
+                </div>
+                <div className="row">
+                    <div class="col-md-12">
+                        <div id="interest0" className="box_interessen" style={{color:"#ffffff", backgroundColor:"#eb9316",
+             fontWeight:"bold", borderRadius:"4px", float:"left", paddingTop:"5px",
+             paddingBottom:"5px", paddingLeft:"15px", paddingRight:"15px",
+             marginTop:"10px", marginBottom:"10px", marginRight:"10px", marginLeft:"0px"}}>Festivals
+                        </div>
+
+                        <div id="interest1" className="box_interessen" style={{color:"#ffffff", backgroundColor:"#eb9316",
+             fontWeight:"bold", borderRadius:"4px", float:"left", paddingTop:"5px",
+             paddingBottom:"5px", paddingLeft:"15px", paddingRight:"15px",
+             marginTop:"10px", marginBottom:"10px", marginRight:"10px", marginLeft:"0px"}}>Festivals
+                        </div>
+
+                        <div id="interest2" className="box_interessen" style={{color:"#ffffff", backgroundColor:"#eb9316",
+             fontWeight:"bold", borderRadius:"4px", float:"left", paddingTop:"5px",
+             paddingBottom:"5px", paddingLeft:"15px", paddingRight:"15px",
+             marginTop:"10px", marginBottom:"10px", marginRight:"10px", marginLeft:"0px"}}>Festivals
+                        </div>
+
+                        <div id="interest3" className="box_interessen" style={{color:"#ffffff", backgroundColor:"#eb9316",
+             fontWeight:"bold", borderRadius:"4px", float:"left", paddingTop:"5px",
+             paddingBottom:"5px", paddingLeft:"15px", paddingRight:"15px",
+             marginTop:"10px", marginBottom:"10px", marginRight:"10px", marginLeft:"0px"}}>Festivals
+                        </div>
+
+                        <div id="interest4" className="box_interessen" style={{color:"#ffffff", backgroundColor:"#eb9316",
+             fontWeight:"bold", borderRadius:"4px", float:"left", paddingTop:"5px",
+             paddingBottom:"5px", paddingLeft:"15px", paddingRight:"15px",
+             marginTop:"10px", marginBottom:"10px", marginRight:"10px", marginLeft:"0px"}}>Festivals
+                        </div>
+
+                        <div id="interest5" className="box_interessen" style={{color:"#ffffff", backgroundColor:"#eb9316",
+             fontWeight:"bold", borderRadius:"4px", float:"left", paddingTop:"5px",
+             paddingBottom:"5px", paddingLeft:"15px", paddingRight:"15px",
+             marginTop:"10px", marginBottom:"10px", marginRight:"10px", marginLeft:"0px"}}>Festivals
+                        </div>
+
+                        <div id="interest6" className="box_interessen" style={{color:"#ffffff", backgroundColor:"#eb9316",
+             fontWeight:"bold", borderRadius:"4px", float:"left", paddingTop:"5px",
+             paddingBottom:"5px", paddingLeft:"15px", paddingRight:"15px",
+             marginTop:"10px", marginBottom:"10px", marginRight:"10px", marginLeft:"0px"}}>Festivals
+                        </div>
+
+                        <div id="interest7" className="box_interessen" style={{color:"#ffffff", backgroundColor:"#eb9316",
+             fontWeight:"bold", borderRadius:"4px", float:"left", paddingTop:"5px",
+             paddingBottom:"5px", paddingLeft:"15px", paddingRight:"15px",
+             marginTop:"10px", marginBottom:"10px", marginRight:"10px", marginLeft:"0px"}}>Festivals
+                        </div>
+
+                        <div id="interest8" className="box_interessen" style={{color:"#ffffff", backgroundColor:"#eb9316",
+             fontWeight:"bold", borderRadius:"4px", float:"left", paddingTop:"5px",
+             paddingBottom:"5px", paddingLeft:"15px", paddingRight:"15px",
+             marginTop:"10px", marginBottom:"10px", marginRight:"10px", marginLeft:"0px"}}>Festivals
+                        </div>
+
+                        <div id="interest9" className="box_interessen" style={{color:"#ffffff", backgroundColor:"#eb9316",
+             fontWeight:"bold", borderRadius:"4px", float:"left", paddingTop:"5px",
+             paddingBottom:"5px", paddingLeft:"15px", paddingRight:"15px",
+             marginTop:"10px", marginBottom:"10px", marginRight:"10px", marginLeft:"0px"}}>Festivals
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    componentDidMount() {
         let profileService = new ProfileService();
 
         profileService.findProfileByUserId(localStorage.getItem("sessionUserId"))
@@ -15,7 +91,7 @@ export default class RowInteressenComponent extends React.Component {
                 let finished = aboutmeParts[0] + "#";
 
                 for (let i = 1; i < 10; i++) {
-                    let selector = "#" + i;
+                    let selector = "#interest" + i;
 
                     if (i < interests.length) {
                         $(selector).text(interests[i]);
@@ -28,89 +104,5 @@ export default class RowInteressenComponent extends React.Component {
             .catch(function (err) {
                 console.log(err);
             });
-
-        return <div>
-            <div className="row">
-                <div className="col-md-12">
-                    <h1>Interessen</h1>
-                    <br />
-                </div>
-            </div>
-            <div className="row">
-                <div class="col-md-12">
-                    <div id="1" className="box_interessen" style={{color:"#ffffff", backgroundColor:"#eb9316",
-             fontWeight:"bold", borderRadius:"4px", float:"left", paddingTop:"5px",
-             paddingBottom:"5px", paddingLeft:"15px", paddingRight:"15px",
-             marginTop:"10px", marginBottom:"10px", marginRight:"10px", marginLeft:"0px"}}>Festivals
-                    </div>
-
-                    <div id="2" className="box_interessen" style={{color:"#ffffff", backgroundColor:"#eb9316",
-             fontWeight:"bold", borderRadius:"4px", float:"left", paddingTop:"5px",
-             paddingBottom:"5px", paddingLeft:"15px", paddingRight:"15px",
-             marginTop:"10px", marginBottom:"10px", marginRight:"10px", marginLeft:"0px"}}>Festivals
-                    </div>
-
-                    <div id="3" className="box_interessen" style={{color:"#ffffff", backgroundColor:"#eb9316",
-             fontWeight:"bold", borderRadius:"4px", float:"left", paddingTop:"5px",
-             paddingBottom:"5px", paddingLeft:"15px", paddingRight:"15px",
-             marginTop:"10px", marginBottom:"10px", marginRight:"10px", marginLeft:"0px"}}>Festivals
-                    </div>
-
-                    <div id="4" className="box_interessen" style={{color:"#ffffff", backgroundColor:"#eb9316",
-             fontWeight:"bold", borderRadius:"4px", float:"left", paddingTop:"5px",
-             paddingBottom:"5px", paddingLeft:"15px", paddingRight:"15px",
-             marginTop:"10px", marginBottom:"10px", marginRight:"10px", marginLeft:"0px"}}>Festivals
-                    </div>
-
-                    <div id="5" className="box_interessen" style={{color:"#ffffff", backgroundColor:"#eb9316",
-             fontWeight:"bold", borderRadius:"4px", float:"left", paddingTop:"5px",
-             paddingBottom:"5px", paddingLeft:"15px", paddingRight:"15px",
-             marginTop:"10px", marginBottom:"10px", marginRight:"10px", marginLeft:"0px"}}>Festivals
-                    </div>
-
-                    <div id="6" className="box_interessen" style={{color:"#ffffff", backgroundColor:"#eb9316",
-             fontWeight:"bold", borderRadius:"4px", float:"left", paddingTop:"5px",
-             paddingBottom:"5px", paddingLeft:"15px", paddingRight:"15px",
-             marginTop:"10px", marginBottom:"10px", marginRight:"10px", marginLeft:"0px"}}>Festivals
-                    </div>
-
-                    <div id="7" className="box_interessen" style={{color:"#ffffff", backgroundColor:"#eb9316",
-             fontWeight:"bold", borderRadius:"4px", float:"left", paddingTop:"5px",
-             paddingBottom:"5px", paddingLeft:"15px", paddingRight:"15px",
-             marginTop:"10px", marginBottom:"10px", marginRight:"10px", marginLeft:"0px"}}>Festivals
-                    </div>
-
-                    <div id="8" className="box_interessen" style={{color:"#ffffff", backgroundColor:"#eb9316",
-             fontWeight:"bold", borderRadius:"4px", float:"left", paddingTop:"5px",
-             paddingBottom:"5px", paddingLeft:"15px", paddingRight:"15px",
-             marginTop:"10px", marginBottom:"10px", marginRight:"10px", marginLeft:"0px"}}>Festivals
-                    </div>
-
-                    <div id="0" className="box_interessen" style={{color:"#ffffff", backgroundColor:"#eb9316",
-             fontWeight:"bold", borderRadius:"4px", float:"left", paddingTop:"5px",
-             paddingBottom:"5px", paddingLeft:"15px", paddingRight:"15px",
-             marginTop:"10px", marginBottom:"10px", marginRight:"10px", marginLeft:"0px"}}>Festivals
-                    </div>
-
-                    <div id="9" className="box_interessen" style={{color:"#ffffff", backgroundColor:"#eb9316",
-             fontWeight:"bold", borderRadius:"4px", float:"left", paddingTop:"5px",
-             paddingBottom:"5px", paddingLeft:"15px", paddingRight:"15px",
-             marginTop:"10px", marginBottom:"10px", marginRight:"10px", marginLeft:"0px"}}>Festivals
-                    </div>
-
-                </div>
-            </div>
-        </div>;
-    }
-
-    render() {
-        return (
-            <div>
-                {this.createRowInteressen()}
-                <br />
-                <br />
-                <hr />
-            </div>
-        );
     }
 }
