162d6b2d6452085163c90686a8b59cec0e015057
Alexej Esau <alexej.esau@fh-bielefeld.de>
2016-07-11 14:01:31 +0200

    Make some cleanups

--- src/js/components/ProfileinstellungenViewProfilLoeschenComponent.js
+++ src/js/components/ProfileinstellungenViewProfilLoeschenComponent.js
@@ -8,25 +8,21 @@ export default class ProfileinstellungenViewProfilLoeschenComponent extends Reac
         return (
             <div>
                 <div className="row">
-                    <br/>
                     <h3>Profil l&ouml;schen</h3>
-                    <div className="col-md-6 col-md-offset-3">
-                        <br/>
+                    <div>
                         <div className="checkbox">
-                            <label><input type="checkbox" id="delete" value=""/>
+                            <label>
+                                <input type="checkbox" id="delete"/>
                                 Ich bin einverstanden damit, dass dies mein Profil und Freundschaften entfernen wird
                             </label>
                         </div>
                     </div>
                 </div>
                 <div className="row">
-                    <div className="col-md-6 col-md-offset-3">
-                        <br/>
-                        <button className="btn btn-danger btn-md btn-block" type="button" onClick={this.deleteProfil()}>
+                    <div>
+                        <button type="button" className="btn btn-danger btn-md btn-block" onClick={this.deleteProfil}>
                             <span className="glyphicon glyphicon-trash"></span> Profil l&ouml;schen
                         </button>
-                        <br/>
-                        <br/>
                     </div>
                 </div>
                 <hr/>
@@ -35,13 +31,10 @@ export default class ProfileinstellungenViewProfilLoeschenComponent extends Reac
     }
 
     deleteProfil() {
-        var id;
-        var accept;
+        let id = localStorage.getItem("sessionUserId");
+        let accept = document.getElementById("delete").checked;
 
-        id = localStorage.getItem("sessionUserId");
-        accept = document.getElementById("delete").checked;
-
-        if (accept === true) {
+        if (accept) {
             let profilService = new ProfilService();
             let registerService = new RegisterService();
 
@@ -61,7 +54,6 @@ export default class ProfileinstellungenViewProfilLoeschenComponent extends Reac
                     //aktuelles profil finden
                     registerService.findIdByName(data[0].login, {
                         success: function (data) {
-
                             //profil entfernen
                             registerService.delete(data[0], {
                                 success: function (data) {
