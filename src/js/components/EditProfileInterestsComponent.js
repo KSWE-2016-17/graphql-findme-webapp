332829753947ed210bc60834bb44c8782ff44b6e
Alexej Esau <alexej.esau@fh-bielefeld.de>
2016-07-11 15:47:17 +0200

    Fix saving interests

--- src/js/components/EditProfileInterestsComponent.js
+++ src/js/components/EditProfileInterestsComponent.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import PS from "../services/ProfilService";
+import ProfileService from "../services/ProfilService";
 
 export default class EditProfileInterestsComponent extends React.Component {
     constructor(props) {
@@ -24,61 +24,61 @@ export default class EditProfileInterestsComponent extends React.Component {
                             <div className="row">
                                 <div className="col-md-2">
                                     <div className="form-group">
-                                        <input id="0" type="text" className="form-control"
+                                        <input id="interest0" type="text" className="form-control"
                                                placeholder="Interesse eingeben"/>
                                     </div>
                                 </div>
                                 <div className="col-md-2">
                                     <div className="form-group">
-                                        <input id="1" type="text" className="form-control"
+                                        <input id="interest1" type="text" className="form-control"
                                                placeholder="Interesse eingeben"/>
                                     </div>
                                 </div>
                                 <div className="col-md-2">
                                     <div className="form-group">
-                                        <input id="2" type="text" className="form-control"
+                                        <input id="interest2" type="text" className="form-control"
                                                placeholder="Interesse eingeben"/>
                                     </div>
                                 </div>
                                 <div className="col-md-2">
                                     <div className="form-group">
-                                        <input id="3" type="text" className="form-control"
+                                        <input id="interest3" type="text" className="form-control"
                                                placeholder="Interesse eingeben"/>
                                     </div>
                                 </div>
                                 <div className="col-md-2">
                                     <div className="form-group">
-                                        <input id="4" type="text" className="form-control"
+                                        <input id="interest4" type="text" className="form-control"
                                                placeholder="Interesse eingeben"/>
                                     </div>
                                 </div>
                                 <div className="col-md-2">
                                     <div className="form-group">
-                                        <input id="5" type="text" className="form-control"
+                                        <input id="interest5" type="text" className="form-control"
                                                placeholder="Interesse eingeben"/>
                                     </div>
                                 </div>
                                 <div className="col-md-2">
                                     <div className="form-group">
-                                        <input id="6" type="text" className="form-control"
+                                        <input id="interest6" type="text" className="form-control"
                                                placeholder="Interesse eingeben"/>
                                     </div>
                                 </div>
                                 <div className="col-md-2">
                                     <div className="form-group">
-                                        <input id="7" type="text" className="form-control"
+                                        <input id="interest7" type="text" className="form-control"
                                                placeholder="Interesse eingeben"/>
                                     </div>
                                 </div>
                                 <div className="col-md-2">
                                     <div className="form-group">
-                                        <input id="8" type="text" className="form-control"
+                                        <input id="interest8" type="text" className="form-control"
                                                placeholder="Interesse eingeben"/>
                                     </div>
                                 </div>
                                 <div className="col-md-2">
                                     <div className="form-group">
-                                        <input id="9" type="text" className="form-control"
+                                        <input id="interest9" type="text" className="form-control"
                                                placeholder="Interesse eingeben"/>
                                     </div>
                                 </div>
@@ -87,7 +87,7 @@ export default class EditProfileInterestsComponent extends React.Component {
                             <div className="row">
                                 <div className="col-md-12">
                                     <div className="form-group">
-                                        <button type="submit" className="btn btn-success"
+                                        <button type="button" className="btn btn-success"
                                                 onClick={this.updateInterests}>
                                             <span className="glyphicon glyphicon-ok"></span> Hinzufügen
                                         </button>
@@ -110,22 +110,21 @@ export default class EditProfileInterestsComponent extends React.Component {
     }
 
     componentDidMount() {
-        let ps = new PS();
+        let profileService = new ProfileService();
 
-        ps.findProfileByUserId(localStorage.getItem("sessionUserId"))
+        profileService.findProfileByUserId(localStorage.getItem("sessionUserId"))
             .then(function (data) {
-                let dummy = data[0].aboutme;
-                let i = dummy.split("#");
-                let interests = i[1].split("+");
+                let aboutme = data[0].aboutme;
+                let aboutmeParts = aboutme.split("#");
+                let interests = aboutmeParts[1].split("+");
 
-                for (let x = 0; x < 10; x++) {
-                    let help = "#" + x;
+                for (let i = 0; i < 10; i++) {
+                    let selector = "#interest" + i;
 
-                    if (x < interests.length) {
-                        $(help).val(interests[x]);
+                    if (i < interests.length) {
+                        $(selector).val(interests[i]);
                     }
                 }
-
             })
             .catch(function (err) {
                 console.log(err);
@@ -133,37 +132,32 @@ export default class EditProfileInterestsComponent extends React.Component {
     }
 
     updateInterests() {
-        let ps = new PS();
+        let profileService = new ProfileService();
 
-        let complete = "#";
+        let interestsBuffer = "#";
 
-        for (let x = 0; x < 10; x++) {
-            let help = "#" + x;
+        for (let i = 0; i < 10; i++) {
+            let selector = "#interest" + i;
 
-            if ($(help).val() === "") {
-
-            } else {
-                var nwo = "+" + $(help).val();
-                complete = complete.concat(nwo);
+            if ($(selector).val() !== "") {
+                interestsBuffer += "+" + $(selector).val();
             }
         }
 
-        ps.findProfileByUserId(localStorage.getItem("sessionUserId"))
+        profileService.findProfileByUserId(localStorage.getItem("sessionUserId"))
             .then(function (data) {
-                let dummy = data[0].aboutme;
-                let parts = dummy.split("{");
-                let about = parts[0] + "{" + complete;
+                let aboutme = data[0].aboutme;
+                let aboutmeParts = aboutme.split("{");
 
-                data[0].aboutme = about;
+                data[0].aboutme = aboutmeParts[0] + "{" + interestsBuffer;
 
-                ps.updateProfile(data[0])
+                profileService.updateProfile(data[0])
                     .then(function (data) {
                         console.log("SUCCESS");
                     })
                     .catch(function (err) {
                         console.log(err);
                     });
-
             })
             .catch(function (err) {
                 console.log(err);
