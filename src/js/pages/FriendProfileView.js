169d4a0fd45bb2ee41c3ad71b111978f4b13a025
Alexej Esau <alexej.esau@fh-bielefeld.de>
2016-12-06 21:33:38 +0100

    Remove commented code

--- src/js/pages/FriendProfileView.js
+++ src/js/pages/FriendProfileView.js
@@ -3,7 +3,6 @@ import React from "react";
 import NavigationBar from "../components/ProfilAnsichtNavigationElement";
 import Head from "../components/FriendProfileView_HeadRow";
 import Pictures from "../components/FriendProfileView_PictureRow";
-//import Interests from "../components/ProfilAnsichtRowInteressen";
 
 import Interests from "../components/FriendProfileInterests";
 
