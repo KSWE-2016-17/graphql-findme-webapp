44b4d8b6e04f0c226c9b44beb96fcc1697d1460b
Alexej Esau <alexej.esau@fh-bielefeld.de>
2016-12-06 17:50:53 +0100

    Remove PictureService
    According to the refactoring of the DB API it is not possible to
    store and also to load pictures in/from database.

--- src/js/components/DefaultImage.js
+++ src/js/components/DefaultImage.js
@@ -1,7 +1,5 @@
 import React from "react";
 
-import PS from "../services/PictureService";
-
 export default class DefaultImageComponent extends React.Component {
 
     createDefaultImage() {
--- src/js/components/FriendProfileImage.js
+++ src/js/components/FriendProfileImage.js
@@ -1,7 +1,5 @@
 import React from "react";
 
-import PS from "../services/PictureService";
-
 export default class FriendProfileImage extends React.Component {
 
     constructor(props) {
@@ -12,39 +10,6 @@ export default class FriendProfileImage extends React.Component {
     }
 
     createDefaultProfilImage() {
-        let ps = new PS();
-
-        let self = this;
-        console.log("profile image view id: " + self.props.profileID);
-        ps.getPictureData(self.props.profileID)
-            .then(function (data) {
-                let url = "http://elbe203.startdedicated.de:15984/findme/";
-
-                console.log("BILD");
-                console.log(data[0]._id);
-                console.log(data[0]);
-
-                url = url + data[0]._id + "/";
-
-                for (let x in data[0]._attachments) {
-                    console.log(x);
-                    let foo = data[0]._attachments[x];
-                    console.log(foo);
-                    url = url + x;
-                    break;
-                }
-
-                console.log("URL FOLGT");
-
-                console.log(url);
-
-                $("#pic").attr("src", url);
-
-            })
-            .catch(function (err) {
-                console.log(err);
-            });
-
         return <img src={'https://b.thumbs.redditmedia.com/GVc0WVhrmqvIQyMolx763ItKqJ0krwqZLOw7nlg03uY.jpg'}
                     alt="default" width="140px" height="140px" className="img-thumbnail" id="pic"/>;
     }
