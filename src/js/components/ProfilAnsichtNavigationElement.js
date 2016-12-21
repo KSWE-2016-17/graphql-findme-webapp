b2e84a5f83d76b317b5f84cf7ab7f932e7defd8f
Alexej Esau <alexej.esau@fh-bielefeld.de>
2016-12-21 18:55:36 +0100

    Clean up code

--- src/js/components/ProfilAnsichtNavigationElement.js
+++ src/js/components/ProfilAnsichtNavigationElement.js
@@ -16,42 +16,63 @@ export default class ProfilAnsichtNavigationElement extends React.Component {
                 <nav className="navbar navbar-default">
                     <div>
                         <div className="navbar-header">
-                            <button type="button" className="navbar-toggle" data-toggle="collapse"
+                            <button className="navbar-toggle" type="button" data-toggle="collapse"
                                     data-target="#myNavbar">
                                 <span className="icon-bar"></span>
                                 <span className="icon-bar"></span>
                                 <span className="icon-bar"></span>
                             </button>
-                            <a className="navbar-brand">findMe</a>
+                            <a className="navbar-brand" role="button">findMe</a>
                         </div>
-                        <div className="collapse navbar-collapse" id="myNavbar">
+
+                        <div id="myNavbar" className="collapse navbar-collapse">
                             <ul className="nav navbar-nav">
-                                <li><a href="#/profile"><span className="glyphicon glyphicon-user"></span> Profil</a>
+                                <li>
+                                    <a href="#/profile">
+                                        <span className="glyphicon glyphicon-user"></span> Profil
+                                    </a>
                                 </li>
-                                <li><a href="#/dating"><span className="glyphicon glyphicon-send"></span> Dating</a>
+                                <li>
+                                    <a href="#/dating">
+                                        <span className="glyphicon glyphicon-send"></span> Dating
+                                    </a>
                                 </li>
-                                <li><a href="#/friendstab"><span className="glyphicon glyphicon-asterisk"></span>
-                                    Freunde</a></li>
+                                <li>
+                                    <a href="#/friendstab">
+                                        <span className="glyphicon glyphicon-asterisk"></span> Freunde
+                                    </a>
+                                </li>
                                 <li className="dropdown">
-                                    <a className="dropdown-toggle" data-toggle="dropdown" href="#"><span
-                                        className="glyphicon glyphicon-envelope"></span> Nachrichten <span
-                                        className="caret"></span></a>
+                                    <a className="dropdown-toggle" role="button" data-toggle="dropdown">
+                                        <span className="glyphicon glyphicon-envelope"></span> Nachrichten
+                                        <span className="caret"></span>
+                                    </a>
                                     <ul className="dropdown-menu">
-                                        <li><a href="#/mails/inbox">Posteingang</a></li>
-                                        <li><a href="#/mails/new">Nachricht erstellen</a></li>
-                                        <li><a href="#/mails/outbox">gesendete Nachrichten</a></li>
-                                        <li><a href="#/mails/archive">Archiv</a></li>
+                                        <li>
+                                            <a href="#/mails/inbox">Posteingang</a>
+                                        </li>
+                                        <li>
+                                            <a href="#/mails/new">Nachricht erstellen</a>
+                                        </li>
+                                        <li>
+                                            <a href="#/mails/outbox">gesendete Nachrichten</a>
+                                        </li>
+                                        <li>
+                                            <a href="#/mails/archive">Archiv</a>
+                                        </li>
                                     </ul>
                                 </li>
                                 <li>
-                                    <form role="form" className="navbar-form navbar-input-group">
+                                    <form className="navbar-form navbar-input-group" role="form">
                                         <div className="input-group">
-                            <span className="input-group-btn">
-                                <button className="btn btn-default btn-sm" type="button" onClick={this.searchUser}><span
-                                    className="glyphicon glyphicon-search"></span></button>
-                            </span>
+                                            <div className="input-group-btn">
+                                                <button className="btn btn-default btn-sm" type="button"
+                                                        onClick={this.searchUser}>
+                                                    <span className="glyphicon glyphicon-search"></span>
+                                                </button>
+                                            </div>
                                             <div className="form-group">
-                                                <input id="searchField" type="text" className="form-control input-sm"
+                                                <input id="searchField" className="form-control input-sm" type="text"
                                                        placeholder="Suche"/>
                                             </div>
                                         </div>
@@ -59,11 +80,15 @@ export default class ProfilAnsichtNavigationElement extends React.Component {
                                 </li>
                             </ul>
                             <ul className="nav navbar-nav navbar-right">
-                                <li><a href="#/settings"><span className="glyphicon glyphicon-wrench"></span>
-                                    Einstellungen</a>
+                                <li>
+                                    <a href="#/settings">
+                                        <span className="glyphicon glyphicon-wrench"></span> Einstellungen
+                                    </a>
                                 </li>
-                                <li><a href="#/logout"><span className="glyphicon glyphicon-log-out"></span>
-                                    Ausloggen</a>
+                                <li>
+                                    <a href="#/logout">
+                                        <span className="glyphicon glyphicon-log-out"></span> Ausloggen
+                                    </a>
                                 </li>
                             </ul>
                         </div>
