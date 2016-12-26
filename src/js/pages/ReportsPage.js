3fb71fdd8ede3d1e19107da23b799fda46870c0d
Alexej Esau <alexej.esau@fh-bielefeld.de>
2016-12-26 12:33:13 +0100

    Refactor reports page

--- src/js/pages/ReportsPage.js
+++ src/js/pages/ReportsPage.js
@@ -1,17 +1,52 @@
 import React from "react";
 
 import Navigation from "../components/Navigation";
-import Head from "../components/ReportsView_HeadRow";
-import ReportsList from "../components/ReportsView_ReportsListRow";
+import ReportListItemComponent from "../components/ReportListItemComponent";
+
+import AdminService from "../services/AdminService";
 
 export default class ReportsPage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            reports: []
+        };
+
+        this.adminService = new AdminService();
+    }
+
     render() {
         return (
             <div>
                 <Navigation/>
-                <Head/>
-                <ReportsList/>
+
+                <h1>Meldungen</h1>
+
+                <div>
+                    {this.state.reports}
+                </div>
             </div>
         );
+    }
+
+    componentDidMount() {
+        this.adminService.allProfiles()
+            .then((data) => {
+                let newReportList = this.state.reports;
+
+                for (let i = 0; i < data.length; i++) {
+                    if (data[i].reported == true) {
+                        newReportList.push(
+                            <ReportListItemComponent profileId={data[i]._id}/>
+                        );
+                    }
+                }
+
+                this.setState({reports: newReportList});
+            })
+            .catch((err) => {
+                console.log(err);
+            });
     }
 }
